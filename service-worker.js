84589a9414e4316f07865096a789497a793dfce0
slatohamid <117818692+slatohamid@users.noreply.github.com>
2025-11-29 22:52:02 +0100

    Refactor service worker event listeners to use modern syntax

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,28 @@
-const CACHE_NAME = 'mak-kalkulator-v1';
+const CACHE_NAME = "mak-kalkulator-v1";
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
+self.addEventListener("install", evt => {
+  evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
+self.addEventListener("activate", evt => {
+  evt.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+self.addEventListener("fetch", evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request))
   );
 });
