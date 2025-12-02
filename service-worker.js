1f650c3741023068c2de59fe5ad99141f09dc8bb
slatohamid <117818692+slatohamid@users.noreply.github.com>
2025-12-03 00:21:30 +0100

    Update cache version and assets in service worker
    Updated cache name to version 4 and added bobex.html to assets.

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,15 @@
-const CACHE_NAME = "mak-kalkulator-v1";
+const CACHE_NAME = "mak-kalkulator-v4";
 const ASSETS = [
   "./",
   "./index.html",
+  "./bobex.html",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", evt => {
+  self.skipWaiting();
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
@@ -17,7 +19,7 @@ self.addEventListener("activate", evt => {
   evt.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
